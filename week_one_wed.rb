81b04d8f7aa07346e8f559f057cbe1d75273bc84
Kilima Glenn <thunderenlight@gmail.com>
2015-05-19 19:55:24 -0500

    Update week_one_wed.rb

--- week_one_wed.rb
+++ week_one_wed.rb
@@ -30,3 +30,56 @@ def prime(n)
 	end
 	 true
 end
+
+
+require 'debugger'
+
+# An array of primes
+# -- Prime:
+        # => Is divisible only by 1 and itself
+# -- To determine prime:
+        # => Test from 1 to (n / 2)
+
+# Requirement:
+# No duplicates in array of primes
+
+
+def prime_factors(number)
+  primes = []
+  return primes unless number >= 1
+  return primes << number if is_prime?(number)
+  # until is_prime?(number) do
+    (2..(number / 2)).to_a.each do |num|
+      if number % num == 0  # Divisor
+        if is_prime?(num)
+          primes << num
+          number /= num
+          return (primes << prime_factors(number)).flatten
+          # break
+        end
+      end
+    end
+  # end
+  # primes << number
+end
+
+def is_prime?(number)
+  (2..(number / 2)).to_a.each do |num|
+    if number % num == 0
+      return false
+    end
+  end
+  true
+end
+
+
+# puts is_prime?(3) == true
+# puts is_prime?(41) == true
+
+p prime_factors(0)          == []
+p prime_factors(3)          == [3]
+p prime_factors(6)            == [2,3]
+p prime_factors(8)          == [2,2,2] # => 2 [=> 4 => [2, 2]]
+p prime_factors(25)         == [5,5]
+p prime_factors(123123123)  == [3, 3, 41, 333667]
+
